e4a7afa5483c8b5e742cb04c41c40dd26370c53b
FabioV <FabioV@Fabio>
2016-04-17 16:50:01 +0200

    Esplicazione classe Set

--- TennisGame/src/tennisgame/controller/Set.java
+++ TennisGame/src/tennisgame/controller/Set.java
@@ -3,6 +3,14 @@ package tennisgame.controller;
 import tennisgame.model.Player;
 
 public class Set extends Game {
+	
+	// Oggetto creato per eventuali sviluppi futuri al momento presente a titolo d' esempio
+	
+	// Esempio di utilizzo:
+	
+	// L' oggetto creato come un' array pari al numero di set da giocare potrebbe contenere
+	// il nome per ognuno degli stessi del vincitore.
+	
 	public Set() {
 		
 	}
